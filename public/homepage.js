3a252ed5be02066d8c3c0b245950459f36bea281
scholarAbe <abraham.saldinas.656@my.csun.edu>
2025-10-09 20:59:05 -0700

    added homepage and about page

---
+++ public/homepage.js
@@ -0,0 +1,9 @@
+const about = document.getElementById("about");
+about.addEventListener('click', function() {
+    window.location.href = "about.html";
+});
+const teams = document.getElementById("teams");
+const proj = document.getElementById("projects");
+const settings = document.getElementById("settings");
+const con = document.getElementById("contacts");
+onclick.window

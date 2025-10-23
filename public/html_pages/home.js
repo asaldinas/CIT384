0cfef77de25035a6958ca16e668831c900109c96
scholarAbe <abraham.saldinas.656@my.csun.edu>
2025-10-22 18:33:09 -0700

    added psuedocode to home.js

--- public/html_pages/home.js
+++ public/html_pages/home.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
     const submitButton = document.getElementById("submit");
+    const user = document.getElementById("username");
+    if (user is null){
+        return message
+    }
+    else{
+        continue;
+    }
+    const password = document.getElementById("password");
+    if (password is null or password not found){
+        return message
+    }
+    else{
+        continue;
+    }
 
     submitButton.addEventListener("click", () => {
         window.location.href = "form.html";
